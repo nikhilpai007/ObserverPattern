8995720f03f834f75ecf2908a84fbc0f37b9ec0f
nikhi <nikhi@DESKTOP-PERE7PS>
2019-03-03 19:43:35 -0500

    Commit to changes

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,13 +6,13 @@ ArrayListSubject COIS3400H = new ArrayListSubject("COIS3400H"); //creating a new
 ArrayListSubject COIS4000H = new ArrayListSubject("COIS4000Y"); //creating a new arraylist named COIS400Y
 
 student Nikhil = new student("Nikhil Pai"); //creating new object for student to add to the arraylist 
-student Sean = new student("Sean Behan"); //creating new object for student to add to the arraylist 
+student Gokhan = new student("Gokhan Karasu"); //creating new object for student to add to the arraylist 
 student Daniel = new student("Daniel Devine"); //creating new object for student to add to the arraylist 
 student Nicholas =  new student("Nicholas Barnes"); //creating new object for student to add to the arraylist 
 student Natasha = new student("Natasha Prytulka"); //creating new object for student to add to the arraylist 
 
 COIS3040H.append(Nikhil); // Appending object to the arraylist 
-COIS3040H.append(Sean); // Appending object to the arraylist 
+COIS3040H.append(Gokhan); // Appending object to the arraylist 
 COIS3400H.append(Nikhil); // Appending object to the arraylist 
 COIS3400H.append(Nicholas); // Appending object to the arraylist 
 COIS4000H.append(Daniel); // Appending object to the arraylist 
